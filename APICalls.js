2ca1af1b3217863001fdcf9a5735446fae7c00e2
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-10-27 15:52:31 -0400

    put APIs in their own module

---
+++ APICalls.js
@@ -0,0 +1,36 @@
+const getMovies = async (cityName) => {
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`)
+    if (response.status === 200) {
+      return response
+    }
+  }
+  catch (error) {
+
+    console.log(error.code)
+    const movieError = error
+    console.log('log')
+    return movieError
+  }
+}
+
+const getWeather = async (latitude, longitude) => {
+  try {
+    const response = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${latitude}&lon=${longitude}`)
+    if (response.status === 200) {
+
+      return response
+    }
+
+  }
+
+  catch (error) {
+
+    console.log(error.code)
+    const weatherError = error
+
+    return weatherError
+  }
+}
+
+exports.module = {getMovies, getWeather}
